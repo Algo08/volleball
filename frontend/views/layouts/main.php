91c5ea9befcd275f4445ec1121df7117b995a45f
Algo08 <salohiddin.esanov@mail.ru>
2021-01-26 11:29:17 +0500

    fix

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -116,10 +116,7 @@ AppAsset::register($this);
 <div class="footer">
     <div class="container">
         <div class="col-xs-4 agileinfo_footer_grid">
-            <h4><?=Yii::t('main','Biz haqimizda')?></h4>
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                <span>Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-            </p>
+            <img class="logo-position" src="<?=Url::to('@web/frontend/web/img/logo1.png')?>" alt="" />
         </div>
         <div class="col-xs-4 agileinfo_footer_grid">
             <h4>Address</h4>
